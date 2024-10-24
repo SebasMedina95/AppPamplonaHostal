8c9427e5374a84dfdf902b0c62fc9ac0c20a2f0a
Sebastian Medina <JSebastian19952011@gmail.com>
2024-10-23 22:30:30 -0500

    Estructura inicial y commit inicial

---
+++ src/main/java/org/sebastian/hostal_pamplona/HostalPamplonaApplication.java
@@ -0,0 +1,13 @@
+package org.sebastian.hostal_pamplona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostalPamplonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostalPamplonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sebastian/hostal_pamplona/HostalPamplonaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebastian.hostal_pamplona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostalPamplonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
